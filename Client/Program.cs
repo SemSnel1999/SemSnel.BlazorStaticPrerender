4f2889a8d37ab07fcfc120d2e943ccb6f8698a6a
Sem Snel <sem.snel@weareyou.com>
2022-03-10 21:26:20 +0100

    update wwwroot

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
