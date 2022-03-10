9fdb2d16f5c5c2dd6db9c38064e5fdf11ac41d57
Sem Snel <sem.snel@weareyou.com>
2022-03-10 21:18:51 +0100

    add static prerender and remove bin and object folders

--- Client.Host/Startup.cs
+++ Client.Host/Startup.cs
@@ -1,80 +1,88 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Client.Host
+namespace Client.Host;
+
+public class Startup
 {
-    public class Startup
+    private readonly IWebHostEnvironment _env;
+    private readonly IConfiguration _config;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Startup" /> class.
+    /// </summary>
+    /// <param name="webHostEnvironment">The web hosting environment.</param>
+    /// <param name="config">The configuration.</param>
+    /// <remarks>
+    /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+    /// </remarks>
+    public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
     {
-        private readonly IWebHostEnvironment _env;
-        private readonly IConfiguration _config;
+        _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup" /> class.
-        /// </summary>
-        /// <param name="webHostEnvironment">The web hosting environment.</param>
-        /// <param name="config">The configuration.</param>
-        /// <remarks>
-        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
-        /// </remarks>
-        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+    /// <summary>
+    /// Configures the services.
+    /// </summary>
+    /// <param name="services">The services.</param>
+    /// <remarks>
+    /// This method gets called by the runtime. Use this method to add services to the container.
+    /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    /// </remarks>
+    /// 
+    public string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.Configure<RazorPagesOptions>(options => options.RootDirectory = "/Pages");
+
+        services.AddSingleton<HttpClient>(sp =>
         {
-            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
-            _config = config ?? throw new ArgumentNullException(nameof(config));
+            // Get the address that the app is currently running at
+            var server = sp.GetRequiredService<IServer>();
+            
+            var addressFeature = server.Features.Get<IServerAddressesFeature>();
+            
+            var  baseAddress = addressFeature.Addresses.First();
+            
+            return new HttpClient { BaseAddress = new Uri(baseAddress) };
+        });
+
+        services.AddRazorPages();
+    }
+
+    /// <summary>
+    /// Configures the application.
+    /// </summary>
+    /// <param name="app">The application builder.</param>
+    /// <param name="env">The web hosting environment.</param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        /// <summary>
-        /// Configures the services.
-        /// </summary>
-        /// <param name="services">The services.</param>
-        /// <remarks>
-        /// This method gets called by the runtime. Use this method to add services to the container.
-        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        /// </remarks>
-        /// 
-        public string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        app.UseHttpsRedirection();
 
-        public void ConfigureServices(IServiceCollection services)
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
         {
-            // Add services to the container.
-            services.Configure<RazorPagesOptions>(options => options.RootDirectory = "/Pages");
-
-            services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri("https://localhost:7256/")
-            });
-
-            services.AddRazorPages();
-        }
-
-        /// <summary>
-        /// Configures the application.
-        /// </summary>
-        /// <param name="app">The application builder.</param>
-        /// <param name="env">The web hosting environment.</param>
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapFallbackToPage("/_Host");
-            });
-        }
+            endpoints.MapRazorPages();
+            endpoints.MapFallbackToPage("/_Host");
+        });
     }
 }
---
+++ Client.StaticPreRenderer/AppTestFixture.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Xunit.Abstractions;
+
+
+namespace Client.StaticPreRenderer;
+
+public class AppTestFixture : WebApplicationFactory<Client.Host.Program>
+{
+    protected override IHostBuilder CreateHostBuilder()
+    {
+        var builder = base.CreateHostBuilder();
+        builder.UseEnvironment(Environments.Production);
+
+        // Replace the HttpClient dependency with a new one
+        builder.ConfigureWebHost(
+            webHostBuilder =>
+            {
+                webHostBuilder.UseStaticWebAssets(); // Add this line
+                webHostBuilder.ConfigureTestServices(services =>
+                {
+                    services.Remove(new ServiceDescriptor(typeof(HttpClient), typeof(HttpClient), ServiceLifetime.Singleton));
+                    services.AddSingleton(_ => CreateDefaultClient());
+                });
+            });
+        
+        return builder;
+    }
+}
