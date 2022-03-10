fc01353dc20a19c16e019106f544393eb246246e
Sem Snel <sem.snel@weareyou.com>
2022-03-10 19:20:50 +0100

    refactor client project

---
+++ Client.Host/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Host
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="webHostEnvironment">The web hosting environment.</param>
+        /// <param name="config">The configuration.</param>
+        /// <remarks>
+        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+        /// </remarks>
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// </remarks>
+        /// 
+        public string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.Configure<RazorPagesOptions>(options => options.RootDirectory = "/Pages");
+
+            services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7256/")
+            });
+
+            services.AddRazorPages();
+        }
+
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The web hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
